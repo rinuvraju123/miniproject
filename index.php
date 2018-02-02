202339e8be71527af7f93ed38a6b40a1375dd3b3
rinuvraju123 <rinuvraju@mca.ajce.in>
2018-02-02 14:46:56 +0530

    commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php include_once("header.php"); ?>
+
+    <form name="f1">
+   <input type="hidden" name="h1" value="0"/>
+    <div class="banner" id="banner">
+    	<div class="wrapper">
+        	<div class="content1">THINK YOU MAY HAVE  <br />HEARING LOSS</div>
+            <div class="content2"><a href="registration.php">Register Now</a></div>
+            <div class="content3">Think you may have hearing loss? Try this quick and easy at-home hearing test <br />simulator to find out if you should talk to a hearing professional. </div>
+        </div>
+    </div></form>
+    <div class="learn">
+    	<div class="wrapper">
+    	<div class="left">
+            <div class="title">TYPES OF HEARING AIDS </div>
+            <div class="sub_title">SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA.</div>
+            <div class="cont">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip. Duis aute irure dolor in reprehenderit. Lorem ipsum dolor sit amet co tetur adipisicing elit.</div>
+        </div>
+        <div class="right">
+        	<div class="but"><a href="view_hearingaid.php">ADVERTISEMENTS</a></div>
+        </div>
+        </div>
+    </div>
+  <div class="qcon">
+    <a href="#"><img src="images/youtube-icon.png" /></a>
+    <div class="connect">
+    	<div class="wrapper">
+        	<div class="round1"><img src="images/round1.png" /></div>
+            <div class="roundcon">
+                    <div class="rtitle">HEAR BETTER <label> LIVE BETTER</label></div>
+                    <div class="rcon">EXPLORE WIRELESS HEARING AID AND <br />DIGITAL HEARING AID STYLES TO FIND THE<br /> ONE THAT'S RIGHT FOR YOU.</div>
+            </div>
+            <div class="round2"><img src="images/round2.png" /></div>
+        </div>
+    </div>
+
+ <?php include_once("footer.php"); ?>
